61480362e6f4f54f3b57fd90780796e37c8622a8
Nagarjuna Reddy Alavala <nagarjunareddyalavala954@gmail.com>
2023-06-02 11:15:32 -0500

    3rd assignment

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,19 @@ public class Main {
         Student s = new Student();
         s.setName("Nagarjuna Reddy");
         System.out.printf("Name of the student: "+ s.getName());
+        //Day 3 assignment
+        Product product = new Product();
+        product.setPrice(12.5);
+        double price = product.getPrice();
+        System.out.println("Price for single quantity: " + price);
+
+        int quantity = 7;
+        double totalPrice = product.getPrice(quantity);
+        System.out.println("Price for " + quantity + " quantities: " + totalPrice);
+
+        //runtime polymorphism
+        Department department = new SalesDepartment(20);
+        int departmentSize = department.getDepartmentSize();
+        System.out.println("Department Size: " + departmentSize);
     }
 }
