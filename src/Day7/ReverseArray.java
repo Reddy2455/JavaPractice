d8a37423ac8ac90debeeb7a15c0bcc7f94c917ac
Nagarjuna Reddy Alavala <nagarjunareddyalavala954@gmail.com>
2023-06-07 15:25:48 -0500

    6th assignment

---
+++ src/Day7/ReverseArray.java
@@ -0,0 +1,16 @@
+package Day7;
+import java.util.*;
+
+public class ReverseArray {
+    public static void main(String[] args){
+        int a[] = new int[]{2,3,4,5,6};
+        int n = a.length;
+
+        for(int i=n-1;i>=0;i--)
+        {
+            System.out.println(a[i]);
+        }
+
+
+    }
+}
